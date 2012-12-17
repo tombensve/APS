582e8921232a599f4dd7e04b834b64eeeaa0b308
Tommy Svensson <tommy@natusoft.se>
2012-12-17 17:17:00 +0100

    comiting files the ide excluded.

---
+++ Archetypes/APSWarBundleArchetype/target/classes/archetype-resources/src/test/java/BundleTest.java
@@ -0,0 +1,35 @@
+import java.util.Properties;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BundleTest {
+    
+    public BundleTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void myTest() throws Exception {
+        
+    }
+    
+}
