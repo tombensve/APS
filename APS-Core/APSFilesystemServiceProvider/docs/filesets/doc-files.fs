309017b3e38235c96528160ea3e228ffcb8cfc75
Tommy Svensson <tommy@natusoft.se>
2013-01-27 13:35:07 +0100

    Documentation.

---
+++ APS-Core/APSFilesystemServiceProvider/docs/filesets/doc-files.fs
@@ -0,0 +1,5 @@
+# I use a "file-set" of documentation file specifications so that it can be reused for building a complete document of
+# APS as a whole.
+
+APS-Core/APSFilesystemServiceProvider/docs/.*.md,
+APS-APIs/src/main/java/se/natusoft/osgi/aps/api/core/filesystem/**/.*.java
