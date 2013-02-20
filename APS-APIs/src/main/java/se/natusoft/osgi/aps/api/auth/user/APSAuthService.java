9fd20a221e025a4da4e511cda65eb5925f0855c3
Tommy Svensson <tommy@natusoft.se>
2013-02-20 18:37:21 +0100

    Dev commit.

--- APS-APIs/src/main/java/se/natusoft/osgi/aps/api/auth/user/APSAuthService.java
+++ APS-APIs/src/main/java/se/natusoft/osgi/aps/api/auth/user/APSAuthService.java
@@ -1,3 +1,39 @@
+/* 
+ * 
+ * PROJECT
+ *     Name
+ *         APS APIs
+ *     
+ *     Code Version
+ *         0.9.0
+ *     
+ *     Description
+ *         Provides the APIs for the application platform services.
+ *         
+ * COPYRIGHTS
+ *     Copyright (C) 2012 by Natusoft AB All rights reserved.
+ *     
+ * LICENSE
+ *     Apache 2.0 (Open Source)
+ *     
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *     
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *     
+ * AUTHORS
+ *     Tommy Svensson (tommy@natusoft.se)
+ *         Changes:
+ *         2013-02-20: Created!
+ *         
+ */
 package se.natusoft.osgi.aps.api.auth.user;
 
 import java.util.Properties;
@@ -8,10 +44,10 @@ import java.util.Properties;
  * <p/>
  * Implementations can lookup the user in an LDAP for example, or use some other user service.
  * <p/>
- * APS supplies an APSSimpleUserServiceAuthenticationServiceProvider that uses the
+ * APS supplies an APSSimpleUserServiceAuthServiceProvider that uses the
  * APSSimpleUserService to authenticate. It is provided in its own bundle.
  */
-public interface APSAuthenticationService<Credentials> {
+public interface APSAuthService<Credentials> {
 
 
     /**
