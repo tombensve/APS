fe1fa42312f5b2874f436231d62d77a34a2b3d85
tommy <tommy@natusoft.se>
2015-12-23 18:28:26 +0100

    Cosmetics.

--- APS-Core/APSConfigServiceProvider/src/main/java/se/natusoft/osgi/aps/core/config/model/admin/APSConfigEditModelImpl.java
+++ APS-Core/APSConfigServiceProvider/src/main/java/se/natusoft/osgi/aps/core/config/model/admin/APSConfigEditModelImpl.java
@@ -3,33 +3,33 @@
  * PROJECT
  *     Name
  *         APS Configuration Service Provider
- *     
+ *
  *     Code Version
  *         1.0.0
- *     
+ *
  *     Description
  *         A more advanced configuration service that uses annotated interfaces to
  *         describe and provide access to configuration. It supports structured
  *         configuration models.
- *         
+ *
  * COPYRIGHTS
  *     Copyright (C) 2012 by Natusoft AB All rights reserved.
- *     
+ *
  * LICENSE
  *     Apache 2.0 (Open Source)
- *     
+ *
  *     Licensed under the Apache License, Version 2.0 (the "License");
  *     you may not use this file except in compliance with the License.
  *     You may obtain a copy of the License at
- *     
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  *     Unless required by applicable law or agreed to in writing, software
  *     distributed under the License is distributed on an "AS IS" BASIS,
  *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
- *     
+ *
  * AUTHORS
  *     tommy ()
  *         Changes:
@@ -204,9 +204,8 @@ public class APSConfigEditModelImpl<APSConfigSubclass extends APSConfig> extends
         this.version = configAnn.version();
         this.configId = configAnn.configId();
         this.group = configAnn.group();
-        getKey().
-                configId(this.configId).
-                version(this.version);
+        // Update the key with configId and version.
+        getKey().configId(this.configId).version(this.version);
 
         for (Field field : this.configClass.getFields()) {
             if (!Modifier.isStatic(field.getModifiers()) && Modifier.isPublic(field.getModifiers())) {
@@ -216,6 +215,7 @@ public class APSConfigEditModelImpl<APSConfigSubclass extends APSConfig> extends
                 }
             }
         }
+
     }
 
     //
