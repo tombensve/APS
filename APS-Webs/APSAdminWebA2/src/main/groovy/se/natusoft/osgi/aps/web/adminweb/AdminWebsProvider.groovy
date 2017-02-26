cbe327d13967e5671e535390f550b30f8a57d2ae
tommy <tommy@natusoft.se>
2017-02-26 19:10:40 +0100

    Merge remote-tracking branch 'origin/aps-100' into aps-100 - WIP commit.

--- APS-Webs/APSAdminWebA2/src/main/groovy/se/natusoft/osgi/aps/web/adminweb/AdminWebsProvider.groovy
+++ APS-Webs/APSAdminWebA2/src/main/groovy/se/natusoft/osgi/aps/web/adminweb/AdminWebsProvider.groovy
@@ -1,24 +1,53 @@
 package se.natusoft.osgi.aps.web.adminweb
 
 import se.natusoft.osgi.aps.apsadminweb.service.APSAdminWebService
+import se.natusoft.osgi.aps.tools.APSLogger
 import se.natusoft.osgi.aps.tools.annotation.activator.Initializer
 import se.natusoft.osgi.aps.tools.annotation.activator.Managed
 import se.natusoft.osgi.aps.tools.annotation.activator.OSGiService
+import se.natusoft.osgi.aps.tools.groovy.lib.MapJsonDocVerifier
 
 /**
  * General services.
  */
 class AdminWebsProvider {
+    //
+    // Constants
+    //
+
+    private static final Map<String, Object> EVENT_SCHEMA = [
+            header_1: [
+                    type_1      : "service",
+                    address_1   : "?aps\\.admin\\..*",
+                    classifier_1: "?public|private"
+            ],
+            body_1  : [
+                    action_1: "get-webs"
+            ],
+            reply_0: [
+                    webs_1: [
+                            [
+                                    name_1: "?.*",
+                                    url_1: "?^https?://.*"
+                            ]
+                    ]
+            ]
+    ] as Map<String, Object>
+
+    private static final MapJsonDocVerifier EVENT_VERIFIER = new MapJsonDocVerifier( validStructure: EVENT_SCHEMA)
 
     //
     // Private Members
     //
 
+    @Managed( loggingFor = "aps-admin-web:admin-webs-provider" )
+    private APSLogger logger
+
     @Managed
     private LocalEventBus localBus
 
     @OSGiService
-    private APSAdminWebService adminWebServcie
+    private APSAdminWebService adminWebService
 
     //
     // Init
@@ -27,7 +56,14 @@ class AdminWebsProvider {
     @Initializer
     void init() {
         this.localBus.consume { Map<String, Object> event ->
-            if (event [ Constants.])
+            try {
+                EVENT_VERIFIER.validate(event)
+
+
+            }
+            catch (Exception e) {
+                this.logger.error("Problem with received event: $e.message")
+            }
         }
     }
 }
