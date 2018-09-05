7a448e48db5e733281d36cc3e070f97a72a842a2
tommy <tommy@natusoft.se>
2018-09-05 18:55:08 +0200

    aps-140 Add webb support for React.js - APSLogger still had old comments.

--- APS-Webs/APSWebManager/src/main/js/aps-webmanager-frontend/src/APSLogger.js
+++ APS-Webs/APSWebManager/src/main/js/aps-webmanager-frontend/src/APSLogger.js
@@ -1,20 +1,9 @@
 /**
- * Logging utility that automatically converts messages to logable type.
+ * Logging utility providing a little bit more info.
  *
  * ### Usage:
  *
- *     this._logger.info/warn/error/debug("Something: {}, something else: {}", [ someObject, "whatever" ]);
- *     this._logger.warn("{} is on a collision course with your house!", ["Santa"] );
- *     this._logger.error("{} ate all candied apples!", "Rudolf" );
- *
- * - All {} will be replaced with the string version of the corresponding value in 'data' array.
- *   The first {} will get the first value in the array and so on.
- *
- * - If the second argument is not an array it will be put in an array of one object. This allows you
- *   to skip the array brackets when there is only one data value.
- *
- * And no I did not write this at Christmas time. Santa and Rudolf was the only things that popped up
- * in my mind, don't ask me whu :-).
+ *   this.logger.info/warn/error/debug(`Something: ${something}, something else: ${else}`);
  */
 export default class APSLogger {
 
