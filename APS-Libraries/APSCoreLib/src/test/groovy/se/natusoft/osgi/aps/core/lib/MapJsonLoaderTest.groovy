7a064bcc2041cf667751551ee85c5c81f33c14ba
Tommy Svensson <tommy@natusoft.se>
2019-07-26 16:18:17 +0200

    aps-171 Add message based services - Added test of recursive include.

--- APS-Libraries/APSCoreLib/src/test/groovy/se/natusoft/osgi/aps/core/lib/MapJsonLoaderTest.groovy
+++ APS-Libraries/APSCoreLib/src/test/groovy/se/natusoft/osgi/aps/core/lib/MapJsonLoaderTest.groovy
@@ -8,6 +8,10 @@ class MapJsonLoaderTest {
     void testLoad() {
         Map<String, Object> json = MapJsonLoader.loadMapJson( "json/top.json", this.class.classLoader )
 
+        // Include
         assert json['include']['Num'] == 34
+
+        // Recursive include
+        assert json['include']['qazwsx']['qaz'] == "QWERTY"
     }
 }
