e3bbb042e6d9231c3cb95b19fae4cc88d6331dea
tommy <tommy@natusoft.se>
2019-10-24 14:22:43 +0200

    APS-210 Stay with OSGi and deploy to docker container rather than vagrant. - Fix of JDK 9+ non backwards compatibility problems.

--- APS-APIs/src/main/java/se/natusoft/osgi/aps/activator/ServiceSetup.java
+++ APS-APIs/src/main/java/se/natusoft/osgi/aps/activator/ServiceSetup.java
@@ -1,57 +1,46 @@
-/*
- *
- * PROJECT
- *     Name
- *         APS APIs
- *     
- *     Code Version
- *         1.0.0
- *     
- *     Description
- *         Provides the APIs for the application platform services.
- *         
- * COPYRIGHTS
- *     Copyright (C) 2012 by Natusoft AB All rights reserved.
- *     
- * LICENSE
- *     Apache 2.0 (Open Source)
- *     
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *     
- *       http://www.apache.org/licenses/LICENSE-2.0
- *     
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- *     
- * AUTHORS
- *     tommy ()
- *         Changes:
- *         2015-01-18: Created!
- *
- */
 package se.natusoft.osgi.aps.activator;
-
-import se.natusoft.annotation.beanannotationprocessor.annotations.Bean;
-import se.natusoft.annotation.beanannotationprocessor.annotations.Property;
 
 import java.util.LinkedList;
 import java.util.Properties;
 
 /**
- * Used to provide config for a service instance.
+ * Originally generated with BeanAnnotation processor, but since jkd 9+ javax.annotation.* resides
+ * in the java.se.ee module and maven refused to accept --add-module java.se.ee, but I realize now
+ * that i probably misspelled maven arg. There is however a point in not doing that since that would
+ * guarantee build failure with any JDK < 9. APS is not dependent on anything above JDK 8! I'm building
+ * with JDK 11 for the moment just to make sure it also works.
+ *
+ * So the solution was to just copy the generated bean and clean it up a bit. This is not a big complex
+ * bean, so I was really lazy using the annotation processor, and in the long run cost me more than
+ * just doing it myself from start :-).
  */
-@SuppressWarnings("PackageAccessibility")
-@Bean(value = {
-        @Property(name="serviceAPIs", type=LinkedList.class , generics=String.class, init="new java.util.LinkedList<>()", description=
-                "The service APIs implemented by the service. Can be null in which case the first implemented interface will be used."),
-        @Property(name="props", type= Properties.class, init="new java.util.Properties()", description=
-                "The properties for a registered service instance. This can be empty, but that defeats the purpose of using this."),
-        @Property(name="serviceInstance", type=Object.class, description=
-                "The instance of the service.")
-}, pure = true)
-public class ServiceSetup extends ServiceSetupBean {}
+@SuppressWarnings("WeakerAccess")
+public class ServiceSetup {
+    public ServiceSetup() {
+    }
+
+    private LinkedList<String> serviceAPIs = new LinkedList<>();
+    public void setServiceAPIs(LinkedList<java.lang.String> value) {
+        this.serviceAPIs = value;
+    }
+    public LinkedList<java.lang.String> getServiceAPIs() {
+        return this.serviceAPIs;
+    }
+
+    private Properties props = new Properties();
+    public void setProps(Properties value) {
+        this.props = value;
+    }
+    public Properties getProps() {
+        return this.props;
+    }
+
+    private Object serviceInstance;
+    public void setServiceInstance(Object value) {
+        this.serviceInstance = value;
+    }
+    public Object getServiceInstance() {
+        return this.serviceInstance;
+    }
+
+}
